59dbfb473f7a67e4e60fb13cc2954ac9105ba70b
Collins Sirwani <sircollo71@gmail.com>
2022-10-05 02:00:02 +0300

    Add on click and delete props

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -7,7 +7,11 @@ class TodoList extends React.Component {
      <ul>
       {this.props.todos.map(todo => (
         // <li key={todo.id}>{todo.title}</li>
-        <TodoItem key={todo.id} todo={todo} />
+        <TodoItem 
+        key={todo.id} 
+        todo={todo} 
+        handleChangeProps={this.props.handleChangeProps}
+        deleteTodoProps={this.props.deleteTodoProps}/>
       ))}
      </ul>
     )
