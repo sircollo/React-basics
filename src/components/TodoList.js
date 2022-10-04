f10e4cfd552b3aa05b506baaf6665da1ac7007ed
Collins Sirwani <sircollo71@gmail.com>
2022-10-05 00:27:19 +0300

    Access state data from Parent and pass it to TodoItem child

---
+++ src/components/TodoList.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import TodoItem from './TodoItem';
+
+class TodoList extends React.Component {
+  render() {
+    return (
+     <ul>
+      {this.props.todos.map(todo => (
+        // <li key={todo.id}>{todo.title}</li>
+        <TodoItem key={todo.id} todo={todo} />
+      ))}
+     </ul>
+    )
+  }
+}
+
+export default TodoList
