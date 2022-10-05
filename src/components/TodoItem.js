3c837b0cbacaca5ea6cb7de5725351043556606b
Collins Sirwani <sircollo71@gmail.com>
2022-10-05 03:49:55 +0300

    Ft: use ternary operator to strike-throgh an item once clicked completed

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react'
 import styles from "./TodoItem.module.css"
 class TodoItem extends Component {
   render() {
+    const completedStyle = {
+      fontStyle: "italic",
+      color: "#595959",
+      opacity: 0.4,
+      textDecoration: "line-through",
+    }
     return (
       <li className={styles.item}>
         <input 
@@ -10,7 +16,8 @@ class TodoItem extends Component {
         checked={this.props.todo.completed}
         onChange={ () => this.props.handleChangeProps(this.props.todo.id)}/>
         <button onClick={()=> this.props.deleteTodoProps(this.props.todo.id)}>Delete</button>
-        {this.props.todo.title}</li>
+        <span style={this.props.todo.completed ? completedStyle : null}>
+        {this.props.todo.title}</span></li>
     )
   }
 }
